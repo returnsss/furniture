2bb47d3035570d7fda5c718de28b5082a4003205
SeungJae Lee <hissjnh@naver.com>
2023-05-07 16:26:15 +0900

    init commit

---
+++ src/test/java/com/teamproject/furniture/FurnitureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamproject.furniture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
