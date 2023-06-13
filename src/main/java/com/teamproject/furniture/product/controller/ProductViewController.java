24c9d0876f7413d9f91858c7cad08984419d5d92
SeungJae Lee <hissjnh@naver.com>
2023-06-13 16:11:21 +0900

    230612/pr 수정

--- src/main/java/com/teamproject/furniture/product/controller/ProductViewController.java
+++ src/main/java/com/teamproject/furniture/product/controller/ProductViewController.java
@@ -9,6 +9,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -26,8 +27,8 @@ public class ProductViewController {
         return "/product/addProduct";
     }
 
-    @GetMapping("/product")
-    public String product(Long productId, Model model){
+    @GetMapping("/{productId}")
+    public String product(@PathVariable Long productId, Model model){
         Product product = productService.getProduct(productId);
         model.addAttribute("product", product);
 
